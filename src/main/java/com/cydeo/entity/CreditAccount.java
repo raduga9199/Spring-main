d6e90226ab3a97e89cc841c650ab43a6ce449262
raduga9199 <raduga9199@gmail.com>
2023-07-05 13:42:21 -0400

    Cars

--- src/main/java/com/cydeo/entity/CreditAccount.java
+++ src/main/java/com/cydeo/entity/CreditAccount.java
@@ -1,7 +1,12 @@
 package com.cydeo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "CreditAccount")
 public class CreditAccount extends Account{
 
     private BigDecimal creditLimit;
